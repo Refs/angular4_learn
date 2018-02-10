7771c59dd0d6c9a84268f25cbe111d10d82cdb69
Refs <lhx1010339808@outlook.com>
2018-02-10 09:36:46 +0800

    。

--- injection/src/app/shared/another-product.service.ts
+++ injection/src/app/shared/another-product.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ProductService, Product } from './product.service';
+import { LoggerService } from './logger.service';
 
 @Injectable()
 // 首先AnotherProductService要实现ProductService,这就意味着前者拥有与后者相同的方法，
 export class AnotherProductService implements ProductService {
-
+  private logger: LoggerService;
   getProduct(): Product {
+
     return new Product(2, 'iphone8', 8777, '17年最新款，最NB的手机' );
   }
 
