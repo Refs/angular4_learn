101f30cd3ddb3b2ca75614c2057d7d91d30e2453
Refs <lhx1010339808@outlook.com>
2018-02-27 08:54:02 +0800

    。

---
+++ componentInteraction/src/app/price-quote/price-quote.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-quote',
+  templateUrl: './price-quote.component.html',
+  styleUrls: ['./price-quote.component.css']
+})
+export class PriceQuoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
